e5652513cd007e3c64e5e00c0a487efc89384286
SliverSliver <1050549879@qq.com>
2018-04-16 00:37:45 +0800

    遍历文件夹下文件，输出所有csv文件的行数

---
+++ AccessDirectory.py
@@ -0,0 +1,29 @@
+import os
+
+
+def get_lines(filename):
+    file = open(filename)
+    lines = file.readlines()
+    lines_count = len(lines)
+    return lines_count
+
+
+def get_directory(file):
+    dir_list = os.listdir(os.getcwd())
+    if len(dir_list):
+        for i in range(0, len(dir_list)):
+            path = os.path.join(os.getcwd(), dir_list[i])
+            if os.path.isfile(path):
+                if path.find(".csv") != -1:
+                    line = dir_list[i] + "," + str(get_lines(path)) + "\n"
+                    file.write(line)
+            elif os.path.isdir(path):
+                os.chdir(path)
+                get_directory(file)
+                os.chdir("../")
+
+
+if __name__ == "__main__":
+    file_list = open("file_list.csv", "w")
+    get_directory(file_list)
+    file_list.close()
--- main.py
+++ main.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
         out_file_name = txt2csv.txt2csv(txt)
         # 按分钟分割csv, 返回目录名
         dir_name = Client.split_by_minutes(out_file_name, 15)
-        print(dir_name) # log.2018-04-14.hour=1
+        # print(dir_name)  # log.2018-04-14.hour=1
         # 按小时分割csv, 返回目录名
-        # dir_name = Client.split_by_hour(out_file_name, 2)
+        dir_name = Client.split_by_hour(out_file_name, 2)
         # 进入分割后目录
-        # os.chdir(dir_name)
+        os.chdir(dir_name)
         # 获取所有csv文件
-        # csv_list = GetFileName.get_all("csv")
-        # for csv in csv_list:
-            # Client.split_by_minutes(csv, 15)
-        # 按请求分割csv
-        # Client.assign_by_request(csv)
+        csv_list = GetFileName.get_all("csv")
+        for csv in csv_list:
+            Client.split_by_minutes(csv, 15)
+            # 按请求分割csv
+            Client.assign_by_request(csv)
         # 返回上级目录
-        # os.chdir("../")
+        os.chdir("../")
