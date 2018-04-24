abc36439ae78ca42417d1ed2214181f4e3202959
SliverSliver <1050549879@qq.com>
2018-04-24 19:00:11 +0800

    实现文件数值合并，逐行数值累加，添加日期功能

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
-if __name__ == "__main__":
+import os
+import numpy as np
+
+
+def replace_csv():
     input_file_name = "temp.csv"
     # output_file_name = input_file_name.replace("localhost_access_", "").replace(".txt", ".csv")
     output_file_name = input_file_name.replace("temp", "temp2")
@@ -12,3 +16,52 @@ if __name__ == "__main__":
                 #     .replace('minutes=00.csv', ':00:00-14:59').replace('minutes=15.csv', ':15:00-29:59')\
                 #     .replace('minutes=30.csv', ':30:00-44:59').replace('minutes=45.csv', ':45:00-59:59')
                 csv_file.write(line)
+
+
+def np_add_csv():
+    for i in range(19, 23):
+        os.chdir("log")
+        csv_1 = np.loadtxt(open("1.info.log.2018-04-" + str(i) + ".count.csv", "rb"), delimiter=",", skiprows=0)
+        csv_2 = np.loadtxt(open("2.info.log.2018-04-" + str(i) + ".count.csv", "rb"), delimiter=",", skiprows=0)
+        csv_3 = np.loadtxt(open("3.info.log.2018-04-" + str(i) + ".count.csv", "rb"), delimiter=",", skiprows=0)
+        csv_4 = csv_1 + csv_2 + csv_3
+        os.chdir("../")
+        np.savetxt("info.log.2018-04-" + str(i + 18) + ".count.csv", csv_4, fmt="%d", delimiter=',')
+
+
+def csv_file_add():
+    csv_1 = np.loadtxt(open("info.log.2018-04-19.count.csv", "rb"), delimiter=",", skiprows=0)
+    csv_2 = np.loadtxt(open("info.log.2018-04-20.count.csv", "rb"), delimiter=",", skiprows=0)
+    csv_3 = np.loadtxt(open("info.log.2018-04-21.count.csv", "rb"), delimiter=",", skiprows=0)
+    csv_4 = np.loadtxt(open("info.log.2018-04-22.count.csv", "rb"), delimiter=",", skiprows=0)
+    csv_5 = np.concatenate((csv_1, csv_2, csv_3, csv_4))
+    np.savetxt("info.log.2018-04-19 - 4-22.count.csv", csv_5, fmt="%d", delimiter=',')
+
+
+def csv_row_add():
+    csv = np.loadtxt(open("info.log.2018-04-19 - 4-22.count.csv", "rb"), delimiter=",", skiprows=0)
+    for i in range(1, len(csv)):
+        csv[i] += csv[i - 1]
+    np.savetxt("info.log.count.csv", csv, fmt="%d", delimiter=',')
+
+
+def csv_add_time():
+    with open("info.log.count.csv", 'r', encoding="utf-8") as csv_file:
+        with open("info.log.csv", 'w', encoding="utf-8") as fileout:
+            time = []
+            for d in range(19, 23):
+                for Hour in range(0, 24):
+                    for Minutes in range(0, 60, 15):
+                        time.append("2018-04-" + str(d).zfill(2) + " " + str(Hour).zfill(2) + ":"
+                                    + str(Minutes).zfill(2) + ":00,")
+
+            i = 0
+            for line in csv_file:
+                line = time[i] + line
+                fileout.write(line)
+                i += 1
+
+
+if __name__ == "__main__":
+    csv_row_add()
+    csv_add_time()
